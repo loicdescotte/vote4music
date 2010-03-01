a548ff664c4257e0922c3bd895675c2dc16a631a
loic.descotte <loic.descotte@f4e7aff0-2383-11df-8b8d-d39edfa62fdf>
2010-03-01 22:08:08 +0000

    form completed
    git-svn-id: https://vote4music.googlecode.com/svn/trunk@9 f4e7aff0-2383-11df-8b8d-d39edfa62fdf

--- vote4Music/app/controllers/Application.java
+++ vote4Music/app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.Album;
+import models.Artist;
 import play.data.validation.Valid;
 import play.mvc.*;
 
@@ -25,11 +26,14 @@ public class Application extends Controller {
         render(album);
     }
 
-    public static void save(@Valid Album album) {
+    public static void save(@Valid Album album, Artist artist) {
+        System.out.println("1");
         if(validation.hasErrors()) {
+            System.out.println("2");
             if(request.isAjax()) error("Invalid value");
             render("@form", album);
         }
+        System.out.println("3");
         album.save();
         list();
     }
