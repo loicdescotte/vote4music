8fb10dc76073a13bafd532191a929138696a9858
loic.descotte <loic.descotte@f4e7aff0-2383-11df-8b8d-d39edfa62fdf>
2010-05-07 12:54:27 +0000

    genre added in list html
    git-svn-id: https://vote4music.googlecode.com/svn/trunk@45 f4e7aff0-2383-11df-8b8d-d39edfa62fdf

--- play/app/controllers/Application.java
+++ play/app/controllers/Application.java
@@ -17,26 +17,41 @@ public class Application extends Controller {
      * List with pagination
      * @param first
      */
-    public static void list(Integer first, String filter) {
+    public static void list(Integer first, String filter, String genre) {
         //number of items to display
         int count = 4;        
         if(first == null){
             first = 0;
         }
-        int total;
-        List<Album> albums;
-        //filter if needed
-        if(filter == null){
-            total = Album.findAll().size();
-            albums= Album.all().from(first).fetch(count);
+        int total=0;
+        List<Album> albums = null;
+        StringBuffer query = new StringBuffer("select a from Album a where (a.name like ? or a.artist.name like ?)");
+        //filter if needed            
+        if(genre!=null){
+            Genre genreEnum = Genre.valueOf(genre.toString().toUpperCase());
+            if(filter == null){                
+                total = Album.find("byGenre",genreEnum ).fetch().size();
+                albums= Album.find("byGenre",genreEnum).from(first).fetch(count);
+            }
+            else{
+                query.append(" and a.genre=?");
+                String queryFitler = "%"+filter+"%";
+                total = Album.find(query.toString(),queryFitler,queryFitler,genreEnum).fetch().size();
+                albums= Album.find(query.toString(),queryFitler,queryFitler,genreEnum).from(first).fetch(count);
+            }
         }
         else{
-            String query = "select a from Album a where a.name like ? or a.artist.name like ?";
-            String queryFitler = "%"+filter+"%";
-            total = Album.find(query,queryFitler,queryFitler).fetch().size();
-            albums= Album.find(query,queryFitler,queryFitler).from(first).fetch(count);
-        }
-        render(albums, first, total, count, filter);
+            if(filter == null){
+                total = Album.findAll().size();
+                albums= Album.all().from(first).fetch(count);
+            }
+            else{
+                String queryFitler = "%"+filter+"%";
+                total = Album.find(query.toString(),queryFitler,queryFitler).fetch().size();
+                albums= Album.find(query.toString(),queryFitler,queryFitler).from(first).fetch(count);
+            }
+         }
+        render(albums, first, total, count, filter, genre);
     }
     
     /**
@@ -68,6 +83,6 @@ public class Application extends Controller {
         //set the album
         album.artist=artist;
         album.save();
-        list(0,null);
+        list(0,null,null);
     }
 }
