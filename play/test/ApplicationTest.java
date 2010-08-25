e41b515401b788957cdb2239cd2d1b5c120b70fd
loic.descotte <loic.descotte@f4e7aff0-2383-11df-8b8d-d39edfa62fdf>
2010-08-25 16:55:54 +0000

    functionnal tests
    git-svn-id: https://vote4music.googlecode.com/svn/trunk@106 f4e7aff0-2383-11df-8b8d-d39edfa62fdf

--- play/test/ApplicationTest.java
+++ play/test/ApplicationTest.java
@@ -1,8 +1,8 @@
+import models.Album;
+import models.Artist;
 import org.junit.*;
 import play.test.*;
-import play.mvc.*;
 import play.mvc.Http.*;
-import models.*;
 
 public class ApplicationTest extends FunctionalTest {
 
@@ -12,6 +12,24 @@ public class ApplicationTest extends FunctionalTest {
         assertIsOk(response);
         assertContentType("text/html", response);
         assertCharset("utf-8", response);
+    }
+    
+    @Test
+    @Ignore
+    public void testArtistisUnique() {
+        //TODO test from UI, JPA does not work from here
+        Artist artist1 = new Artist("joe");
+        Album album1 = new Album("coolAlbum");
+        album1.setArtist(artist1);
+        album1.save();
+        //warning : name must be unique
+        Artist artist2 = new Artist("joe");
+        Album album2 = new Album("coolAlbum2");
+        album2.setArtist(artist2);
+        album2.save();
+
+        //check artist is unique
+        assert(Artist.find("byName").fetch().size()==1);
     }
     
 }
