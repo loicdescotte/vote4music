4810d8f191b52c816febaca1a9723e3de480a688
Loic Descotte <loic.descotte@gmail.com>
2011-04-17 09:07:14 +0200

    PopulateOnStart job

---
+++ app/PopulateOnStart.java
@@ -0,0 +1,17 @@
+import play.*;
+import play.jobs.*;
+import play.test.*;
+ 
+import models.*;
+ 
+@OnApplicationStart
+public class PopulateOnStart extends Job {
+ 
+    public void doJob() {
+        // Check if the database is empty
+        if(Album.count() == 0) {
+            Fixtures.load("init-data.yml");
+        }
+    }
+ 
+}
